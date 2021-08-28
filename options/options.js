e2b7c0efcfd9369cf1361d00c1e2a6f2843df14a
Crowdbotics <team@crowdbotics.com>
2021-08-28 06:33:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "worx_30158Identifier",
+  url: "https://worx_30158Identifier.botics.co",
+  api: "https://worx_30158Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
